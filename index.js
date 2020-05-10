ac539addb4477483065ce9ead035af788375c6f1
Resetium <ajwaite06@gmail.com>
2020-05-10 07:30:37 -0700

    make the mother's day popup

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
+var modal = $("#modal-mothers-day");
 $(document).ready(function() {
     $.getJSON("http://jsonip.com/?callback=?", function(data) {
         console.log(data);
         var d = new Date();
         if (data.ip = "2601:603:1a80:c430:3923:c3e2:7665:a656" && d.getMonth() == 4 && d.getDay() == 0 && d.getDate() > 7 && d.getDate() < 14) {
-            mothersDay();
+            modal.style.display = "block";
             alert("Happy Mother's Day!");
         }
     });
 });
 
-function mothersDay() {
-
+var span = $("#modal-mothers-day-close");
+span.onclick = () => {
+    modal.style.display = none;
+}
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
