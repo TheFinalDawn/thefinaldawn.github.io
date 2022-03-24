8b15b6b19d030e3acd76279a8e93554a7b1f6c84
Resetium <ajwaite06@gmail.com>
2022-03-23 17:47:09 -0700

    Rewrite this site (kind of) so it fits me more

--- index.js
+++ index.js
@@ -18,7 +18,18 @@ function processRequest(e) {
     }
 }
 window.onclick = function(event) {
-    if (event.target == document.getElementById("modal-mothers-day")) {
-        document.getElementById("modal-mothers-day").style.display = "none";
+        if (event.target == document.getElementById("modal-mothers-day")) {
+            document.getElementById("modal-mothers-day").style.display = "none";
+        }
     }
+    /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("navigation").style.width = "250px";
+    //document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("navigation").style.width = "0";
+    //document.getElementById("main").style.marginLeft = "0";
 }
